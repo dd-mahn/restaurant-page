03a799d44dfa691df94a10f67569fb137689a328
godsadeser <ducmanh0801@gmail.com>
2024-01-16 16:16:31 +0700

    add logo and clean code

--- src/header.js
+++ src/header.js
@@ -1,38 +1,31 @@
-export default function header(){
-    const headerBar = document.createElement('div')
-    headerBar.classList.add('header')
+import logo from './images/logo.svg';
 
-    const headerLogo = document.createElement('div')
-    headerLogo.classList.add('header__logo')
+export default function header() {
+    const headerBar = document.createElement('div');
+    headerBar.classList.add('header');
 
-    const headerNav = document.createElement('div')
-    headerNav.classList.add('header__nav')
+    const headerLogo = document.createElement('div');
+    headerLogo.classList.add('header__logo');
+    headerLogo.innerHTML = `<img src="${logo}" alt="Logo">`;
 
-    const nav1 = document.createElement('div')
-    nav1.classList.add('header__nav-item','nav1')
-    const span1 = document.createElement('span')
-    span1.innerHTML = 'Home'
-    nav1.appendChild(span1)
+    const headerNav = document.createElement('div');
+    headerNav.classList.add('header__nav');
 
-    const nav2 = document.createElement('div')
-    nav2.classList.add('header__nav-item','nav2')
-    const span2 = document.createElement('span')
-    span2.innerHTML = 'Menu'
-    nav2.appendChild(span2)
+    const navItems = [
+        { class: 'nav1', text: 'Home' },
+        { class: 'nav2', text: 'Menu' },
+        { class: 'nav3', text: 'About' }
+    ];
 
+    navItems.forEach(item => {
+        const navItem = document.createElement('div');
+        navItem.classList.add('header__nav-item', item.class);
+        navItem.innerHTML = `<span>${item.text}</span>`;
+        headerNav.appendChild(navItem);
+    });
 
-    const nav3 = document.createElement('div')
-    nav3.classList.add('header__nav-item','nav3')
-    const span3 = document.createElement('span')
-    span3.innerHTML = 'About'
-    nav3.appendChild(span3)
+    headerBar.appendChild(headerLogo);
+    headerBar.appendChild(headerNav);
 
-
-    headerNav.appendChild(nav1)
-    headerNav.appendChild(nav2)
-    headerNav.appendChild(nav3)
-
-    headerBar.appendChild(headerLogo)
-    headerBar.appendChild(headerNav)
-    return headerBar
+    return headerBar;
 }
