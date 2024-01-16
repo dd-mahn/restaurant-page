40dbf7fd10ace1d5da890900303f2c929a32c5e6
godsadeser <ducmanh0801@gmail.com>
2024-01-16 16:27:17 +0700

    Add hero section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './style.css'
 import header from './header.js'
+import hero from './hero.js'
 
 const content = document.getElementById('content')
 const headerBar = header()
+const heroSection = hero()
 content.appendChild(headerBar)
+content.appendChild(heroSection)
