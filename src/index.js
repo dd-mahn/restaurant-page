03c80fe7363983dc521745afd1a015603853d2a9
godsadeser <ducmanh0801@gmail.com>
2024-01-17 20:55:39 +0700

    Import menu module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import './style.css'
 import header from './header.js'
 import hero from './hero.js'
+import menu from './menu.js'
 
 const content = document.getElementById('content')
 const headerBar = header()
 const heroSection = hero()
+const menuSection = menu()
+
 content.appendChild(headerBar)
 content.appendChild(heroSection)
+content.appendChild(menuSection)
+
+heroSection.classList.add('d-off')
