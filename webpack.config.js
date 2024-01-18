d85eb10a4ea8a6e6c7f4b7cc36e818d9a3b057bd
godsadeser <ducmanh0801@gmail.com>
2024-01-18 20:32:10 +0700

    Complete config

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,17 @@ module.exports = {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
             },
+            // {
+            //     test: /\.(png|svg|jpg|gif)$/,
+            //     use: [
+            //       {
+            //         loader: 'file-loader',
+            //         options: {
+            //           name: 'images/[name].[ext]',
+            //         },
+            //       },
+            //     ],
+            //   },
         ]
     }
 
